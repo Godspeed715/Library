13811e5befbd983409995019836a521144ec6866
Godspeed715 <chimzagamdaniel@gmail.com>
2025-09-12 11:18:45 +0100

    modified index.html, style.css and script.js
    added addBook button funtionality

--- script.js
+++ script.js
@@ -8,7 +8,29 @@ function Book(title, author, pages, read, id){
     return `${this.title} by ${this.author}, ${pages} pages, ${read} yet.`
   }
 }
+
+function clearBookContainer(){
+  document.querySelectorAll(".book-container div").forEach(element => {
+    element.remove()
+  })
+}
+function refreshLibrary(){
+  myLibrary.forEach(element => {
+    var book_contents = `
+    Title: ${element.title} \n
+    Author: ${element.author} \n
+    No of Pages: ${element.pages}\n
+    Reading Status: ${element.read}
+    `
+    var div = document.createElement('div')
+    div.innerText = book_contents
+    bookContainer.appendChild(div)
+});
+}
+
 const myLibrary = [];
+const bookContainer = document.querySelector(".book-container")
+
 function addBookToLibrary(title, author, pages, read) {
     new_id = crypto.randomUUID()
     myLibrary.push(new Book(title, author, pages, read, new_id))
@@ -17,16 +39,31 @@ function addBookToLibrary(title, author, pages, read) {
 addBookToLibrary("man", "goliath", 233, "not yet")
 addBookToLibrary("pan", "goliath", 233, "not yet")
 console.log(myLibrary);
-
-const bookContainer = document.querySelector(".book-container")
-myLibrary.forEach(element => {
-    var book = element.title
-    var div = document.createElement('div')
-    div.innerText = book
-    bookContainer.appendChild(div)
-});
+refreshLibrary()
 
 
+
+
+const dialogue = document.getElementById('modal')
+document.querySelector('#addBook').addEventListener("click", (event) => {
+  dialogue.showModal()
+})
+
+document.getElementById('bookForm').addEventListener('submit', (event) => {
+  event.preventDefault()
+  bookTitle = document.getElementById('bookTitle').value
+  bookAuthor = document.getElementById('bookAuthor').value
+  bookPages = document.getElementById('bookPages').value
+  bookReadStatus = document.getElementById('bookReadStatus').value
+
+  addBookToLibrary(bookTitle, bookAuthor, bookPages, bookReadStatus)
+  dialogue.close()
+
+  clearBookContainer()
+  refreshLibrary()
+  console.log(myLibrary);
+  event.target.reset()
+})
 
 
 
